645fc149973340cdbd37995c90136a9535e18c1b
tanjim-cringebot <tanjim.hussain.sajin@g.bracu.ac.bd>
2024-10-31 00:34:12 +0600

    Minor config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      unoptimized: true,
+    },
+    output: 'export', // Keep this if you want to export your app statically
+  };
+  
+  export default nextConfig;
+  
